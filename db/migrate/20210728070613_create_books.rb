55e217d63339311608d25d6d5f96d82f0362a9de
chacolate33 <ko7ta7ko7ta@gmail.com>
2021-08-07 10:32:07 +0000

    [Update] 不必要なカラムの削除

--- db/migrate/20210728070613_create_books.rb
+++ db/migrate/20210728070613_create_books.rb
@@ -3,7 +3,6 @@ class DeviseCreateBooks < ActiveRecord::Migration[5.2]
     create_table :books do |t|
       t.text :title
       t.text :body
-      t.string :book_id
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_29_062910) do
+ActiveRecord::Schema.define(version: 2021_08_07_094806) do
 
   create_table "books", force: :cascade do |t|
     t.text "title"
@@ -30,7 +30,6 @@ ActiveRecord::Schema.define(version: 2021_07_29_062910) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "book_id"
     t.string "profile_image_id"
     t.string "introduction"
     t.index ["email"], name: "index_users_on_email", unique: true
