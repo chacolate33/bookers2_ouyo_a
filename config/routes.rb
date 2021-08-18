41ce5906736e13e063e43eb66f7ab9796fb853cc
chacolate33 <ko7ta7ko7ta@gmail.com>
2021-08-18 11:21:45 +0000

    [Add] フォロー機能view以外作成

--- config/routes.rb
+++ config/routes.rb
@@ -9,10 +9,17 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :books, only: [:new, :create, :index, :show, :destroy, :update, :edit] do
+    
     resources :book_comments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]
   
   end
     
-  resources :users, only: [:show, :edit, :update]
+  resources :users, only: [:show, :edit, :update] do
+    
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
+  
 end
